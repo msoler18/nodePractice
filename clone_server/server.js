94cbaff33626488c93363cc0d3cf833a5b49f79c
msoler18 <msoler735@gmail.com>
2018-09-24 21:52:21 -0500

    Encapsule and create module

--- clone_server/server.js
+++ clone_server/server.js
@@ -1,9 +1,12 @@
 var server = require('http');
-function runServer(require,response){
-  console.log("Someone is connected!");
-  response.writeHead(200,{"Content-Type":"text/html"});
-  response.write("<h1>Server Running</h1>");
-  response.end();
+function begin(){
+  function runServer(require,response){
+    console.log("Someone is connected!");
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write("<h1>Server Running</h1>");
+    response.end();
+  }
+  server.createServer(runServer).listen(8888);
 }
 
-server.createServer(runServer).listen(8888);
+exports.begin = begin;
